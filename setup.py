e5644995f94eb80916e782cdb9c17d4240622616
Stan <lishnih@gmail.com>
2013-08-31 10:57:45 +0400

    Исправил несовместимость с Питон 3 в dump.py. Обновил setup.py.

--- setup.py
+++ setup.py
@@ -13,36 +13,38 @@ else:
     if py_version < (2, 6):
         raise RuntimeError('On Python 2, Index requires Python 2.6 or better')
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 try:
     README = open(os.path.join(here, 'README.md')).read()
     CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 except IOError:
     README = CHANGES = ''
 
-install_requires = [
-    ]
 
-from pickler.lib.info import __version__
+from pickler.lib.info import __pkgname__, __description__, __version__
+
 
 if __name__ == '__main__':
     setup(
-        name = 'pickler',
+        name = __pkgname__,
+        description = __description__,
         version = __version__,
+        long_description = __doc__,
+
         author = 'Stan',
         author_email = 'lishnih@gmail.com',
         url = 'http://github.com/lishnih/pickler',
-        packages = ['pickler', 'pickler/lib'],
-#         scripts = [
-#             'scripts/run_pickler.py',
-#         ],
-        package_data = dict(settings=[]),
-        description = 'Pickle Viewer',
-        long_description = __doc__,
-        platforms = 'any',
         license = 'Public Domain',
+        platforms = 'any',
         keywords = ['Pickle', 'Viewer', 'Tk'],
-        install_requires = install_requires,
+
+        packages = [__pkgname__, __pkgname__+'/lib'],
+        package_data = {__pkgname__: []},
+        scripts = [
+        ],
+        install_requires = [
+        ],
+
         classifiers = [
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
